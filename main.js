acfb3518422642f4ebf20c1a32a542a2fa877233
Alex Huynh <alexhuynh@Alexs-MacBook-Pro.local>
2024-05-21 22:45:58 -0700

    changed color

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ renderer.render(scene, camera);
 
 // Torus
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
+const material = new THREE.MeshStandardMaterial({ color: 0x6e9fe0 });
 const torus = new THREE.Mesh(geometry, material);
 
 scene.add(torus);
